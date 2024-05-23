d6c89a92b3bcd02d94ea9915e3ac3883303ac819
Vyach12 <slava.gusarov.73@mail.ru>
2024-05-23 11:55:52 +0300

    [feat]: init project

---
+++ src/main/java/com/dzoms/user_settings_service/UserSettingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dzoms.user_settings_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSettingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSettingsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzoms/user_settings_service/UserSettingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzoms.user_settings_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSettingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
